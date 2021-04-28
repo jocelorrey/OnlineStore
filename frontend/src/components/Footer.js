0f2e03a30fd5ab27b1e97445cda74eb970bd57e2
jocelorrey <jocelorrey@gmail.com>
2021-04-27 20:16:06 -0400

    Style the footer using react boostrap & css

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,19 @@
 // Created using rafce shortcut
 // (react arrow function component export )
 import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer>
-            A footer!
-        </footer>
+      <footer>
+        <Container>
+          <Row>
+            <Col className="text-center py-3">
+              A footer!
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     )
 }
 

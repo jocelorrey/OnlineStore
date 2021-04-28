2f839f1a0531bed4a6ca7c69c031bb3e546a1906
jocelorrey <jocelorrey@gmail.com>
2021-04-28 07:05:43 -0400

    Customize the navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,22 @@
 // Created using rafce shortcut
 // (react arrow function component export )
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Joce's Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/login">Sign In</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   )
